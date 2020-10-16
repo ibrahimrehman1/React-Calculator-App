61927e9436e2fec1e7fcd93796d150c233565151
Ibrahim Rehman <ibrahimrehman0346@gmail.com>
2020-10-16 18:01:58 +0500

    React Calculator App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Button, Input} from "@material-ui/core"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      currentValue: ""
+    }
+    this.change = this.change.bind(this);
+    this.giveAnswer = this.giveAnswer.bind(this);
+    this.arr = ["+", "-", "*", "/"];
+  }
+
+  change(event){  
+      let state = false;
+      let value = null;
+      if (event.target.value){
+        if (event.target.value.length < this.state.currentValue.length){
+          this.setState({currentValue: event.target.value.length >= 1 ? this.state.currentValue.slice(0, this.state.currentValue.length-1):""})
+          return
+        }
+      }
+      else if (!event.target.value && !event.target.innerText){
+        this.setState({currentValue: ""})
+        return
+      }
+      if (!event.target.value){
+        value = event.target.innerText;
+      }else{
+        value = event.target.value[event.target.value.split("").length - 1];
+      }
+      if (value === "+" || value === "-" || value === "*" || value === "/"){
+        for (let i in this.arr){
+          state = this.state.currentValue.endsWith(this.arr[i]);
+          if (state){
+            break;
+          }
+        }
+      }
+      if ((+value || this.state.currentValue || value === ".") && state === false){
+        this.setState({
+          currentValue: this.state.currentValue.concat(value)
+        })
+      }
+      
+  }
+
+  giveAnswer(){
+    if (this.state.currentValue){
+      let ans = String(eval(this.state.currentValue));
+      this.setState({
+        currentValue: ans === "0" ? "" : ans,
+      })
+    }
+  }
+
+  render(){
+    let value = "25%";
+    return (
+      <main className="main-div">
+        <div className="container">
+          <Input onChange={this.change} value={this.state.currentValue} style={{borderBottom: "1px solid rgba(63, 81, 181, 0.5)", width: "100%", marginBottom: "10px"}}/>
+          <br />
+            
+            {["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", ".", "0", "Ans", "+"].map((obj, index)=>{
+              if (obj !== "Ans"){
+                return(
+                <React.Fragment key={index}>
+                  <Button type="button" variant="outlined" color="primary" style={{fontWeight: "bolder", width: value, margin: ".01px"}} onClick={this.change}>{obj}</Button>
+                  {(obj === "/" || obj === "*" || obj === "-" || obj === "+") ? <br /> : null}
+                </React.Fragment>
+              )
+            }else{
+                return(<Button key={index} type="button" variant="outlined" color="primary" style={{fontWeight: "bolder", minWidth: value, margin: ".01px"}} onClick={this.giveAnswer}>{obj}</Button>)
+            }
+              })}
+            <Button type="button" variant="outlined" color="primary" style={{fontWeight: "bolder", width: "100%", margin: ".01px"}} onClick={()=> this.setState({currentValue: ""})}>Reset</Button>
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
